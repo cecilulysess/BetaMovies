1a80c47fbe5b48cf9cb27a22eade28177744d3cf
Yanxiang Wu <cosmobserver@gmail.com>
2012-03-27 15:50:38 +0800

    initial commit with files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaMovies::Application.config.secret_token = '48fb03f1ea8b3725f4d4c7669c5e65f067e12215878937be2a71b19fc979878c84ae7eb6935bc83170ad8a5dbfcf135c6f18eafb1ae94d8f646d7fb62214ea8b'
