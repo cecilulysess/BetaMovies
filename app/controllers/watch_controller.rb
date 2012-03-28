dc8f4e33e02fa69d2a90b1ff0e632040fbc10327
Yanxiang Wu <cosmobserver@gmail.com>
2012-03-28 20:49:47 +0800

    add watch and episodes

---
+++ app/controllers/watch_controller.rb
@@ -0,0 +1,8 @@
+class WatchController < ApplicationController
+  def index
+  end
+  
+  def show_episode(episode_id = 1)
+    
+  end
+end
---
+++ test/unit/helpers/watch_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class WatchHelperTest < ActionView::TestCase
+end
