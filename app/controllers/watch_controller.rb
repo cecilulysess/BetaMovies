3b52c36a0cecea6c4ebb3d1b398c3729f671660a
Yanxiang Wu <cosmobserver@gmail.com>
2012-03-29 13:31:21 +0800

    ver0.0.5 finished able to display all episodes and play movies

--- app/controllers/watch_controller.rb
+++ app/controllers/watch_controller.rb
@@ -3,8 +3,13 @@ class WatchController < ApplicationController
     @movies = Movie.all
   end
   
-  def show_episode(episode_id)
-    @episode = Episode.find(episode_id)
+  def show_episode
+    @episode = Episode.find(params[:episode_id])
+    
+    @movie = Movie.find(@episode.movie_id)
+    @movie_title = 
+      @movie.title
     @all_episodes = Movie.find(@episode.movie_id).episode
+    
   end
 end
