754e63a27e8e21635036328b458e50cb3da7f3c5
Yanxiang Wu <cosmobserver@gmail.com>
2012-04-11 19:07:04 +0800

    set up new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaMovies::Application.config.secret_token = 'f3d66bfc9eda9cfd1d213681626571ded164e1e9545b7425d4dd0c225c126c8e21e6d51e88bbb87c4f160d77a8bd6ce8d4e00e02b468d4cdd46f39e116c74fb4'
