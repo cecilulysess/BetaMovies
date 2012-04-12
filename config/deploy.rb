bb61c047744a3d6ed0c315817a77d8bc2366db83
Yanxiang Wu <cosmobserver@gmail.com>
2012-04-12 18:47:37 +0800

    add cap files

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+set :user, 'root'
+set :domain, 'bm.yanxiang.me' 
+set :application, 'opt/webapp/project_depo/depo'
+
+set :application, "BetaMovies"
+set :repository,  "#{user}@#{domain}:/opt/webapp/project_depo/depo.git"
+
+set :deploy_to, "/opt/webapp/home/bm.yx.me/"
+
+set :scm, "git"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+
+set :deploy_via, :remote_cache
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=production"
+  end
+end
+
+after "deploy:update_code", :bundle_install
+desc 'install prerequisites'
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install"
+end
